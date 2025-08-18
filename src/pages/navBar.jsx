edc3a373da1995fa8bd35147012a81ca14e1765b
zinhle <ngwanejeanette3@gmail.com>
2025-08-18 22:56:12 +0200

    edited navbar

--- src/pages/navBar.jsx
+++ src/pages/navBar.jsx
@@ -3,13 +3,23 @@ import { Link } from "react-router-dom";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <ul>
+    <nav className="bg-purple-600 text-white px-6 py-4 shadow-md">
+      <ul className="flex space-x-6">
         <li>
-          <Link to="/homePage">Home</Link>
+          <Link
+            to="/homePage"
+            className="hover:text-gray-200 font-semibold transition"
+          >
+            Home
+          </Link>
         </li>
         <li>
-          <Link to="/createBlog">Create</Link>
+          <Link
+            to="/createBlog"
+            className="hover:text-gray-200 font-semibold transition"
+          >
+            Create
+          </Link>
         </li>
       </ul>
     </nav>
